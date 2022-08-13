cba51b6faa12e534e1e44f40f71631c37257c029
osmangold <osmanahmeddahab@gmail.com>
2022-08-14 09:59:44 +1400

    This E-commerce App

---
+++ Flutter-Complete-e-commerce-master/lib/Pages/not_found.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class NotFound extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Not found'),
+          Center(
+            child: FlatButton(
+              child: Text('Back'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
